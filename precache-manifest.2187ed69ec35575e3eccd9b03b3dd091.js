e68caf08146a9706270a963f07f5af8c58b47269
Brad Engelhardt <brad.engelhardt@gmail.com>
2018-10-11 14:45:06 -0700

    Updates

---
+++ precache-manifest.2187ed69ec35575e3eccd9b03b3dd091.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "241a908499ecbc4e27d841c63b9a0bd7",
+    "url": "/static/media/notify_me.241a9084.png"
+  },
+  {
+    "revision": "5da3d2407eda74d8b9f9",
+    "url": "/static/css/main.94c496a5.chunk.css"
+  },
+  {
+    "revision": "6f77e56bc20412bcac4c337d9ce2fb09",
+    "url": "/static/media/xfaire.6f77e56b.png"
+  },
+  {
+    "revision": "38a92ad0b9e854568296",
+    "url": "/static/js/1.38a92ad0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9be8e27d419eeba569114b825ec6edf7",
+    "url": "/static/media/after_school.9be8e27d.png"
+  },
+  {
+    "revision": "5da3d2407eda74d8b9f9",
+    "url": "/static/js/main.5da3d240.chunk.js"
+  },
+  {
+    "revision": "e92894f6527b54e834c8c90b39d81d74",
+    "url": "/static/media/eggplant_invaders.e92894f6.png"
+  },
+  {
+    "revision": "546d6cde5755d3fbeceb14657484deec",
+    "url": "/static/media/tinymessage.546d6cde.png"
+  },
+  {
+    "revision": "1eb4f0688b10507071c35fd552f12062",
+    "url": "/static/media/machete.1eb4f068.png"
+  },
+  {
+    "revision": "38a92ad0b9e854568296",
+    "url": "/static/css/1.9ad38912.chunk.css"
+  },
+  {
+    "revision": "bd9bf0776e4025e0f04b07616cc51003",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/bradengelhardt/precache-manifest.b224ea7374d4416c79be6db3a2016242.js"
+  "/precache-manifest.2187ed69ec35575e3eccd9b03b3dd091.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/bradengelhardt/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
